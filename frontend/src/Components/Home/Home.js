139548228314c01bffb0f3b4f0b3ae12363cee1d
Grant Moore <gmoore@makpar.com>
2021-09-30 08:26:26 -0400

    add basic containerized react app

---
+++ frontend/src/Components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className="d-flex flex-column align-items-center" style={{width: "100%"}}>
+            <h2 className="mt-3">Home</h2>
+            <h4>Some Stuff Goes Here</h4>
+        </div>
+    )
+}
