21c880837c4711b4c11b0d028d70aa2fb7c3c671
Grant Moore <gmoore@makpar.com>
2021-12-15 08:41:31 -0500

    LAB-3 adding globale styles, updating host, testing login componet (it works)

--- frontend/src/app/services/host.service.ts
+++ frontend/src/app/services/host.service.ts
@@ -10,6 +10,6 @@ export class HostService {
   // TODO: parameterize host based on environment.ts file, i.e.
   //        change host to dev, staging or prod based on environment.
   public getHost(){
-    return 'https://innolab-dev.makpar-innovation.net'
+    return 'https://api-innolab-dev.makpar-innovation.net'
   }
 }
