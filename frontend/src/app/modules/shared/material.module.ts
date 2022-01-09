1fe22e36b499f4160dd709e1f25ffb0f7e0719fb
Grant Moore <gmoore@makpar.com>
2022-01-09 13:42:20 -0500

    LAB-132 remove unnecessary imports

--- frontend/src/app/modules/shared/material.module.ts
+++ frontend/src/app/modules/shared/material.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox'; 
 import { MatChipsModule } from '@angular/material/chips';
@@ -12,57 +11,45 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input'; 
 import { MatListModule } from '@angular/material/list'
 import { MatProgressBarModule } from '@angular/material/progress-bar'; 
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatSnackBarModule } from '@angular/material/snack-bar'; 
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatTooltipModule } from '@angular/material/tooltip'; 
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [],
   imports: [
-    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
     MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatListModule,
-    MatProgressSpinnerModule,
     MatProgressBarModule,
     MatSidenavModule,
     MatSnackBarModule,
     MatToolbarModule,
     MatTooltipModule,
     MatExpansionModule,
-    MatPaginatorModule,
   ],
   exports: [
-    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
     MatCheckboxModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatListModule,
-    MatProgressSpinnerModule,
     MatProgressBarModule,
     MatSidenavModule,
     MatSnackBarModule,
     MatToolbarModule,
     MatTooltipModule,
     MatExpansionModule,
-    MatPaginatorModule,
   ]
 })
 export class MaterialModule { }
