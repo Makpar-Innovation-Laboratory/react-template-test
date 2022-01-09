fac5702a57502cac5a5f3a72e2cefa89f5ae6e10
Grant Moore <gmoore@makpar.com>
2022-01-09 13:43:11 -0500

    LAB-132 remove unnecessary imports

--- frontend/src/app/modules/shared/material.module.ts
+++ frontend/src/app/modules/shared/material.module.ts
@@ -6,10 +6,8 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule }  from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list'
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatInputModule } from '@angular/material/input'; 
-import { MatListModule } from '@angular/material/list'
 import { MatProgressBarModule } from '@angular/material/progress-bar'; 
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatSnackBarModule } from '@angular/material/snack-bar'; 
