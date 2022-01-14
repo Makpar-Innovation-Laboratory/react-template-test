c2ae46b4d594bab0e00f5df726b928bfcafbeab4
Grant Moore <gmoore@makpar.com>
2022-01-14 10:55:08 -0500

    LAB-72 mock animations

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SessionStorageService } from 'ngx-webstorage';
 import { AuthService } from 'src/app/services/auth.service';
@@ -29,6 +30,7 @@ describe('HomeComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
+        NoopAnimationsModule
       ],
       providers: [
         { provide: SessionStorageService, useValue: mockSessionStorage }
