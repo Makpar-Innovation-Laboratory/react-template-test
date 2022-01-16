cbcefac90fb274369a5adf942a8ba7ed98b20ec8
Grant Moore <gmoore@makpar.com>
2022-01-15 22:03:01 -0500

    LAB-132 slide triggers

--- frontend/src/app/modules/admin/components/profile/profile.component.ts
+++ frontend/src/app/modules/admin/components/profile/profile.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationPeriods, Animations } from 'src/animations';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  animations: [
+    Animations.getSlideTrigger(AnimationPeriods.short)
+  ]
 })
 export class ProfileComponent implements OnInit {
 
